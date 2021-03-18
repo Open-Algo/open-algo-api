b12f954f1be2c03c6ff1c4cecdf28f32a9525ef3
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-17 20:39:42 -0400

    Extend User Controller with addProblem mutation

---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,103 @@
+module.exports = {
+  async addProblem(email, problemId) {
+    try {
+      const user = await strapi.plugins[
+        "users-permissions"
+      ].services.user.fetch({
+        email,
+      });
+
+      const problem = await strapi.services.problem.findOne({ id: problemId });
+
+      const problemsBefore = user.problems;
+
+      const problemsByDifficultyBefore = user.problemsByDifficulty || {
+        easy: {
+          _total: 0,
+        },
+        medium: {
+          _total: 0,
+        },
+        hard: {
+          _total: 0,
+        },
+        veryHard: {
+          _total: 0,
+        },
+      };
+      const problemsByGroupBefore = user.problemsByGroup || {
+        Array: {
+          _total: 0,
+        },
+        Binary: {
+          _total: 0,
+        },
+        "Binary Search Tree": {
+          _total: 0,
+        },
+        "Binary Tree": {
+          _total: 0,
+        },
+        "Dynamic Programming": {
+          _total: 0,
+        },
+        Graph: {
+          _total: 0,
+        },
+        Heap: {
+          _total: 0,
+        },
+        Interval: {
+          _total: 0,
+        },
+        "Linked List": {
+          _total: 0,
+        },
+        Matrix: {
+          _total: 0,
+        },
+        Stack: {
+          _total: 0,
+        },
+        String: {
+          _total: 0,
+        },
+        Trie: {
+          _total: 0,
+        },
+      };
+
+      const isNotSolvedBefore = !problemsBefore.some(
+        (p) => p.id === problem.id
+      );
+
+      if (isNotSolvedBefore) {
+        problemsBefore.push(problem.id);
+
+        problemsByDifficultyBefore[problem.difficulty][problem.id] = true;
+        problemsByDifficultyBefore[problem.difficulty]._total += 1;
+
+        problemsByGroupBefore[problem.group.tag][problem.id] = true;
+        problemsByGroupBefore[problem.group.tag]._total += 1;
+      }
+
+      const queue = [
+        { problemsByDifficulty: problemsByDifficultyBefore },
+        { problemsByGroup: problemsByGroupBefore },
+        { problems: problemsBefore },
+      ];
+
+      while (queue.length > 0) {
+        const update = queue.shift();
+
+        await strapi
+          .query("user", "users-permissions")
+          .update({ id: user.id }, update);
+      }
+
+      return user;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
